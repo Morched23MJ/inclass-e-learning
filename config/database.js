10a53fe4840edaa6dec9672c479e9cfe167d79ac
Morchid Chellali <morchid.chellali98@gmail.com>
2020-10-06 23:54:13 +0100

    Linked MongoDB Atlas

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb+srv://morched23mj:morched23mj@inclass213.tzxt2.mongodb.net/inclass213?retryWrites=true&w=majority',
     secret: 'shhhh'
 }
