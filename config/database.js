f7f0ae0a9e2361fe52ebec13feaeeb62f00687a3
Morchid Chellali <morchid.chellali98@gmail.com>
2020-10-07 17:30:10 +0100

    Commit the whole project

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb+srv://morched23mj:morched23mj@inclass213.tzxt2.mongodb.net/inclass213?retryWrites=true&w=majority',
+    database: 'mongodb://localhost:27017/meanauth',//'mongodb+srv://morched23mj:morched23mj@inclass213.tzxt2.mongodb.net/inclass213?retryWrites=true&w=majority',
     secret: 'shhhh'
 }
